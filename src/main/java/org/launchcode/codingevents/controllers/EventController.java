4a921e5355fe38c96e0872a24f30fbc115f01dc7
Micah <micahnyoung95@gmail.com>
2022-05-21 11:41:56 -0500

    committing exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -16,26 +18,40 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-
     private static List<String> events = new ArrayList<>();
+//    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
+
+//    model.addAttribute("events", events);
+//    return "events/index";
+
+       events.put("Gamer Coders 4 Life", "You Already Know Bro");
+        events.put("Female Coders 4 Life", "You Already Know Broette");
+        events.put("Animal Coders 4 Life", "You Already Know Animal?");
+        System.out.println(events);
         model.addAttribute("events", events);
         return "events/index";
-    }
+}
 
+//lives at /events/create
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+    public String renderCreateEventForm() {
         return "events/create";
     }
 
-    @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
+    //BROKEN: Data binding not working. Template is being generated, but not filled with data.
+    @GetMapping("events-with-description")
+    public String renderEventsWithDescription() {
+        return "events/events-with-description";
     }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
+        events.put(eventName, eventDescription);
+        return "redirect:/events";
+    }
+
 
 }
